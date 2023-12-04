fb54f2d0c89e1307730791a3eea1345665bc1542
ing.dacastillop <ing.dacastillop@gmail.com>
2023-12-04 12:04:03 -0500

    :arrow_up: Upgrade dependency @rolster/types

--- rollup.config.js
+++ rollup.config.js
@@ -1,30 +1,30 @@
+import typescript from '@rollup/plugin-typescript';
 import commonjs from '@rollup/plugin-commonjs';
 import resolve from '@rollup/plugin-node-resolve';
-import typescript from '@rollup/plugin-typescript';
 
 const plugins = [
   commonjs(),
   resolve(),
   typescript({
-    tsconfig: './tsconfig.json',
+    tsconfig: './tsconfig.app.json',
     declaration: true,
     declarationDir: 'dist',
     include: ['node_modules/@rolster/types/index.d.ts']
   })
 ];
 
-const rollupTs = (name) => {
+const rollupTs = (file) => {
   return {
-    input: `dist/esm/${name}.js`,
+    input: [`dist/esm/${file}.js`],
     output: [
       {
-        file: `dist/cjs/${name}.js`,
+        file: `dist/cjs/${file}.js`,
         format: 'cjs',
         sourcemap: true,
         inlineDynamicImports: true
       },
       {
-        file: `dist/es/${name}.js`,
+        file: `dist/es/${file}.js`,
         format: 'es',
         sourcemap: true,
         inlineDynamicImports: true
@@ -35,4 +35,6 @@ const rollupTs = (name) => {
   };
 };
 
-export default [rollupTs('index')];
+const exports = ['index'];
+
+export default [...exports.map((file) => rollupTs(file))];
