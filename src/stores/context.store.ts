34d77ecad81c4ac7249c74dc3adc04b07d162ef7
ing.dacastillop <ing.dacastillop@gmail.com>
2024-10-20 10:40:09 -0500

    :bento: Improve Project for implementation

--- src/stores/context.store.ts
+++ src/stores/context.store.ts
@@ -1,10 +1,17 @@
 import { AbstractContext } from '../types';
 
-export class Context<K = string>
-  extends Map<K, any>
-  implements AbstractContext<K>
-{
+export class Context<K = string> implements AbstractContext<K> {
+  private store: Map<K, any> = new Map();
+
+  public set(key: K, value?: any): void {
+    this.store.set(key, value);
+  }
+
   public request<T = any>(key: K): Undefined<T> {
-    return this.get(key);
+    return this.store.get(key);
+  }
+
+  public has(key: K): boolean {
+    return this.store.has(key);
   }
 }
--- src/types/context.type.ts
+++ src/types/context.type.ts
@@ -1,3 +1,7 @@
 export interface AbstractContext<K = string> {
+  set(key: K, value?: any): void;
+
   request<T = any>(key: K): Undefined<T>;
+
+  has(key: K): boolean;
 }
